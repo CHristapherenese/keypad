f0d2d0d516824bd856dda664020c363314564012
CHristapherenese <64864560+CHristapherenese@users.noreply.github.com>
2020-10-12 09:27:48 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,59 +1,11 @@
-function _4_NumberSet () {
-    if (R1 == 1 && C1 == 1) {
-        keyboard.type("1")
-    }
-    if (R1 == 1 && C2 == 1) {
-        keyboard.type("2")
-    }
-    if (R1 == 1 && C3 == 1) {
-        keyboard.type("3")
-    }
-    if (R2 == 1 && C1 == 1) {
-        keyboard.type("4")
-    }
-    if (R2 == 1 && C2 == 1) {
-        keyboard.type("5")
-    }
-    if (R2 == 1 && C3 == 1) {
-        keyboard.type("6")
-    }
-    if (R3 == 1 && C1 == 1) {
-        keyboard.type("7")
-    }
-    if (R3 == 1 && C2 == 1) {
-        keyboard.type("8")
-    }
-    if (R3 == 1 && C3 == 1) {
-        keyboard.type("9")
-    }
-    if (R4 == 1 && C2 == 1) {
-        keyboard.type("0")
-    }
+function WriteDPins () {
+    pins.D5.digitalWrite(false)
+    pins.D6.digitalWrite(false)
+    pins.D9.digitalWrite(false)
+    pins.D10.digitalWrite(false)
 }
 function enter () {
     keyboard.type("")
-}
-function DriverAction () {
-    if (R1 == 1 && C4 == 1) {
-        EnableChat()
-        keyboard.type("!EOL")
-        _4_NumberSet()
-    }
-    if (R2 == 1 && C4 == 1) {
-        EnableChat()
-        keyboard.type("!black")
-        _4_NumberSet()
-    }
-    if (R3 == 1 && C4 == 1) {
-        EnableChat()
-        keyboard.type("!dq")
-        _4_NumberSet()
-    }
-    if (R4 == 1 && C4 == 1) {
-        EnableChat()
-        keyboard.type("!clear")
-        _4_NumberSet()
-    }
 }
 function EnableChat () {
     keyboard.key("t", KeyboardKeyEvent.Press)
@@ -69,89 +21,183 @@ function clear () {
     C3 = 0
     C4 = 0
 }
-function ScanDelay () {
-    pause(100)
-}
 input.buttonD12.onEvent(ButtonEvent.Click, function () {
     EnableChat()
     keyboard.type("!yellow")
     enter()
     pins.D13.digitalWrite(true)
 })
+function DrvActnScan () {
+    pins.A4.digitalWrite(false)
+    if (input.buttonD5.isPressed()) {
+        EnableChat()
+        keyboard.type("!EOL")
+        Scan = 1
+    } else {
+        R1 = 0
+    }
+    if (input.buttonD6.isPressed()) {
+        EnableChat()
+        keyboard.type("!black")
+        Scan = 1
+    } else {
+        R2 = 0
+    }
+    if (input.buttonD9.isPressed()) {
+        EnableChat()
+        keyboard.type("!dq")
+        Scan = 1
+    } else {
+        R3 = 0
+    }
+    if (input.buttonD10.isPressed()) {
+        EnableChat()
+        keyboard.type("!clear")
+        Scan = 1
+    } else {
+        R4 = 0
+    }
+}
 function ButtonScanCol () {
-    pins.D5.digitalWrite(false)
-    pins.D6.digitalWrite(false)
-    pins.D9.digitalWrite(false)
-    pins.D10.digitalWrite(false)
     if (input.buttonA1.isPressed()) {
         C1 = 1
-        ScanRow = 1
+        Scan = 2
     } else {
         C1 = 0
     }
     if (input.buttonA2.isPressed()) {
         C2 = 1
-        ScanRow = 1
+        Scan = 2
     } else {
         C2 = 0
     }
     if (input.buttonA3.isPressed()) {
         C3 = 1
-        ScanRow = 1
+        Scan = 2
     } else {
         C3 = 0
     }
     if (input.buttonA4.isPressed()) {
         C4 = 1
-        ScanRow = 1
+        Scan = 2
     } else {
         C4 = 0
     }
 }
+function NumberSet () {
+    if (R1 == 1 && C1 == 1) {
+        keyboard.type("1")
+        clear()
+        Scan = 1
+    }
+    if (R1 == 1 && C2 == 1) {
+        keyboard.type("2")
+        clear()
+        Scan = 1
+    }
+    if (R1 == 1 && C3 == 1) {
+        keyboard.type("3")
+        clear()
+        Scan = 1
+    }
+    if (R2 == 1 && C1 == 1) {
+        keyboard.type("4")
+        clear()
+        Scan = 1
+    }
+    if (R2 == 1 && C2 == 1) {
+        keyboard.type("5")
+        clear()
+        Scan = 1
+    }
+    if (R2 == 1 && C3 == 1) {
+        keyboard.type("6")
+        clear()
+        Scan = 1
+    }
+    if (R3 == 1 && C1 == 1) {
+        keyboard.type("7")
+        clear()
+        Scan = 1
+    }
+    if (R3 == 1 && C2 == 1) {
+        keyboard.type("8")
+        clear()
+        Scan = 1
+    }
+    if (R3 == 1 && C3 == 1) {
+        keyboard.type("9")
+        clear()
+        Scan = 1
+    }
+    if (R4 == 1 && C2 == 1) {
+        keyboard.type("0")
+        clear()
+        Scan = 1
+    }
+}
 function ButtonScanRow () {
-    pins.A1.digitalWrite(false)
-    pins.A2.digitalWrite(false)
-    pins.A3.digitalWrite(false)
-    pins.A4.digitalWrite(false)
     if (input.buttonD5.isPressed()) {
         R1 = 1
-        DriverAction()
+        Scan = 3
     } else {
         R1 = 0
     }
     if (input.buttonD6.isPressed()) {
         R2 = 1
-        DriverAction()
+        Scan = 3
     } else {
         R2 = 0
     }
     if (input.buttonD9.isPressed()) {
         R3 = 1
-        DriverAction()
+        Scan = 3
     } else {
         R3 = 0
     }
     if (input.buttonD10.isPressed()) {
         R4 = 1
-        DriverAction()
+        Scan = 3
     } else {
         R4 = 0
     }
 }
-let ScanRow = 0
+function WriteAPins () {
+    pins.A1.digitalWrite(false)
+    pins.A2.digitalWrite(false)
+    pins.A3.digitalWrite(false)
+    pins.A4.digitalWrite(false)
+}
 let C4 = 0
-let R4 = 0
-let R3 = 0
-let R2 = 0
 let C3 = 0
 let C2 = 0
 let C1 = 0
+let R4 = 0
+let R3 = 0
+let R2 = 0
 let R1 = 0
+let Scan = 0
 pins.D11.digitalWrite(false)
+Scan = 0
+clear()
 forever(function () {
-    if (ScanRow == 0) {
+    if (Scan == 0) {
+        DrvActnScan()
+    } else if (Scan == 1) {
+        WriteAPins()
         ButtonScanCol()
-    } else {
+    } else if (Scan == 2) {
+        WriteDPins()
         ButtonScanRow()
+    } else if (Scan == 3) {
+        NumberSet()
+    } else {
+        for (let index = 0; index < 3; index++) {
+            pins.D13.digitalWrite(true)
+            pause(500)
+            pins.D13.digitalWrite(false)
+            pause(500)
+        }
+        pause(2000)
     }
 })
